82ca1f92c1fcd19d57c23a9a86785f80d54bab6c
AlejandroGimenoBosonit <alejandro.gimeno@bosonit.com>
2022-12-22 11:24:37 +0100

    authentication

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
+import { User } from 'src/app/shared/models/User';
 import { CartService } from '../../../services/cart.service';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -9,18 +12,29 @@ import { CartService } from '../../../services/cart.service';
 export class HeaderComponent implements OnInit {
 
   cartQty: number = 0;
-
+  user!: User;
+  
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private userService: UserService 
   ) {
-    cartService
+    this.cartService
     .getCartObservable()
     .subscribe((newCart)=>{
       this.cartQty = newCart.totalCount;
-    })
+    });
+
+    this.userService.userObservable.subscribe((newUser) => {
+      console.log(newUser);
+      
+      this.user = newUser;
+    });
   }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.userService.logout();
+  }
 }
